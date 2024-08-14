87cfe6450961907242b748067485da6d842b7836
toppop7 <65695818+toppop7@users.noreply.github.com>
2024-08-14 12:09:54 -0500

    Using new system

--- main2.py
+++ main2.py
@@ -11,6 +11,36 @@ def swap(i1,i2):
     b[i1], b[i2] = b[i2], b[i1]
 
 
+def check_basics(res):
+    if res == "output":
+        with open("breakdown.txt",'w',encoding='utf-8') as put:
+                put.write("\n".join(b))
+                put.close()
+    if res == "tierify":
+         c = []
+         for index, value in enumerate(b):
+              layer = math.floor(math.log2(index + 1))
+              c.append(f'{layer} -> {value}')
+         with open("output.txt",'w',encoding='utf-8') as put:
+                put.write("\n".join(c))
+                put.close()
+
+
+
+def climbify(startindex = None):
+    index1 = random.randint(0,len(b) - 1) if startindex == None else startindex
+    parentindex = math.floor((index1 - 1) / 2)
+    print("child: ", b[index1])
+    print("parent: ", b[parentindex])
+    res = input("type 1 to swap parent with child: ")
+    if res == "1":
+       swap(index1,parentindex)
+       climbify(parentindex)
+    if res == "":
+       climbify(None)
+    check_basics(res)
+    
+
 def heapify():
     index1 = random.randint(0,len(b) - 1)
     parentindex = math.floor((index1 - 1) / 2)
@@ -20,19 +50,7 @@ def heapify():
     res = input("type 1 to swap parent with child: ")
     if res == "1":
        swap(index1,parentindex)
-    if res == "output":
-        with open("breakdown.txt",'w',encoding='utf-8') as put:
-                put.write("\n".join(b))
-                put.close()
-    if res == "tierify":
-         c = []
-         for index, value in enumerate(b):
-              layer = math.floor(math.log2(index + 1))
-              c.append([layer,value])
-         with open("output.txt",'w',encoding='utf-8') as put:
-                put.write("\n".join(c))
-                put.close()
+    check_basics(res)
+    
 
-
-while True:
-    heapify()
+climbify()
