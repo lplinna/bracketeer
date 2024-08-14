0b483af8f1f723204bb7339f8fb3692f194e7b09
toppop7 <doofenschmad@gmail.com>
2024-08-14 01:09:51 -0500

    More experimentation with heapify

---
+++ main2.py
@@ -0,0 +1,27 @@
+import random
+import subprocess
+import math
+
+b = []
+with open("list_exp.txt",'r',encoding='utf-8') as gets:
+    b = gets.read().splitlines()
+
+
+def swap(i1,i2):
+    b[i1], b[i2] = b[i2], b[i1]
+
+def heapify():
+    index1 = random.randint(0,len(b))
+    parentindex = math.floor((index1 - 1) / 2)
+    print("child: ", b[index1])
+    print("parent: ", b[parentindex])
+    res = input("type 1 to swap parent with child")
+    if res == "1":
+       swap(index1,parentindex)
+    if res == "output":
+        with open("breakdown.txt",'w',encoding='utf-8') as put:
+                put.write("\n".join(b))
+                put.close()
+
+while True:
+    heapify()
